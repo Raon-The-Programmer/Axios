f9e06f9d4ee7aa2efa7947411f0c1a8e7e2c871a
partha2001ps <parthapn2017@gmail.com>
2023-10-27 19:31:37 +0530

    commit

--- src/component/DeleteUser.jsx
+++ src/component/DeleteUser.jsx
@@ -15,6 +15,7 @@ function DeleteUser({ allData,setAllData }) {
     <div>  <div className="row">
     {allData.map((user) => (
       <div key={user.id} className='card col-12 col-md-4 col-lg-2 m-4 p-4 bg-info'>
+          <div className='card-title'>Name: {user.name}</div>
             <div className='card-title'>User Name: {user.username}</div>
             <div className='card-title'>Email:{user.email}</div>
             <div>Phone: {user.phone}</div>
