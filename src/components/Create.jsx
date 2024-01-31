5fda4b6b0227f114dd4abc53dbfb577582b3ae40
Rahul John <72382015+Raon-The-Programmer@users.noreply.github.com>
2024-01-31 19:33:43 +0530

    Update Create.jsx

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,51 +1,71 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-
-const Create = () => {
-  const [name,setName] = useState('')
-  const [email,setEmail] = useState('')
-  const [phone,setPhone] = useState('')
-  let userObject = {
-    
- name:name,
- email:email,
- phone:phone
-
-  }
-  
-  const handleSubmit=async(e)=>{
-      e.preventDefault()
-      try {
-        const res = await axios.post('https://jsonplaceholder.typicode.com/users',userObject);
-        console.log('User added:', res.data);
-        // You can update the user list here if needed.
-      }
-      catch(err){
-        console.log("Error occured while posting: ",err)
-      }
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
+
+function CreateUser({ allData,setAllData }) {
+  const [newUser, setNewUser] = useState([])
+  const navigate=useNavigate()
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  const addnewUser = async() => {
+    try {
+      const response = await axios.post('https://jsonplaceholder.typicode.com/users', newUser)
+      setAllData([...allData, response.data]);
+      setNewUser({ name: '', username: '', email: '' ,phone:''});
+    navigate('/')
+    }
+    catch (error) {
+      console.log(error)
+    }
   }
   return (
-  <div>
-    <form action="submit" >
-      <div>
-      <label htmlFor="name"> Name: &nbsp;&nbsp;
-        <input type="text" placeholder='Enter your name.' onChange={e=>setName(e.target.value)}/>
-      </label></div>
-      <div>
-      <label htmlFor="email"> Email: &nbsp;&nbsp;
-        <input type="text" placeholder='Email..' onChange={e=>setEmail(e.target.value)} />
-      </label></div>
-      <div>
-      <label htmlFor="phone"> Phone Number: &nbsp;&nbsp;
-        <input type="text" placeholder='type..' onChange={e=>setPhone(e.target.value)}/>
-      </label></div>
-      <button action='submit' onClick={handleSubmit}>Create</button>
-    </form>
-    {}
-  </div>
-  )
+    <div className='m-3'>
+      <div className='row'>
+        <div className='col-md-6 col-sm-12'>
+          <label >Name:</label>
+          <input type='text' className='form-control'
+            name='name'
+            value={newUser.name}
+            onChange={handleChange}
+                  />
+        </div>
+        <div className='col-md-6 col-sm-12'>
+          <label>User Name:</label>
+          <input type='text'
+            name='username'
+            value={newUser.username}
+            onChange={handleChange}
+            className='form-control' />
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-6 col-sm-12'>
+          <label>Email:</label>
+          <input type='text'
+            name='email'
+            value={newUser.email}
+            onChange={handleChange}
+            className='form-control' />
+        </div>
+        <div className='col-md-6 col-sm-12'>
+          <label >Phone:</label>
+          <input type='text'
+            name='phone'
+            value={newUser.phone}
+            onChange={handleChange}
+            className='form-control' />
+        </div>
+      </div>
+      <div className='row'>
+          </div>
+          <button className='btn btn-primary btn-block m-4' onClick={addnewUser} >Add New User</button>
+    </div>
+  );
 }
 
-export default Create;
+export default CreateUser;
