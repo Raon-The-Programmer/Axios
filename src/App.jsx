56f4530e19f582b6d6e25210776f3b1b49f42d31
Rahul John <72382015+Raon-The-Programmer@users.noreply.github.com>
2024-01-31 19:38:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,54 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Routes,BrowserRouter as Router,Route,Link } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
-import Create from './components/Create'
-import Edit from './components/Edit'
-import './App.css'
-import View from './components/View'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-const App = () => { 
+import View from './component/View';
+import CreateUser from './component/CreateUser';
+import DeleteUser from './component/DeleteUser';
+import EditUser from './component/EditUser';
+
+function App() {
+  const [allData, setAllData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setAllData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <Router>
-        <div>
-          <Link to='/' style={{paddingRight:'15px'}}>Dashboard</Link>
-          <Link to='/create' style={{paddingRight:'15px'}}>Create</Link>
-          <Link to='/edit' style={{paddingRight:'15px'}}>Update and Delete</Link>
-          <Link to='/view' style={{paddingRight:'15px'}}>View</Link>
-        </div>
-        <Routes>
-          <Route path='/' element={<Dashboard/>}/>
-          <Route path='/create' element={<Create/>}/>
-          <Route path='/edit' element={<Edit/>}/>
-          <Route path='/view' element={<View/>}/>
-        </Routes>
-      </Router>
-    
-    </div>
-  )
+    <Router>
+      <div className="container">
+        <ul className="nav nav-tabs mt-4">
+          <li className="nav-item">
+            <Link to='/' className='nav-link'>Read Data</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/create' className='nav-link'>Create New User</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/delete' className='nav-link'>Delete User</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/edit' className='nav-link'>Edit User</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path='/' element={<View allData={allData} />} />
+        <Route path='/create' element={<CreateUser allData={allData} setAllData={setAllData} />} />
+        <Route path='/delete' element={<DeleteUser allData={allData} setAllData={setAllData} />} />
+        <Route path='/edit' element={<EditUser allData={allData} setAllData={setAllData} />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
