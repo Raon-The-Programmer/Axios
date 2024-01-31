46104fddff926c5ba389148e9c8e4630d397648f
Rahul John <72382015+Raon-The-Programmer@users.noreply.github.com>
2024-01-31 19:34:48 +0530

    Update and rename Dashboard.css to DeleteUser.jsx

---
+++ src/components/DeleteUser.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import React from 'react'
+
+function DeleteUser({ allData,setAllData }) {
+     
+  const  handleDelete = async(userid) => {
+      try {
+          const response = await axios.delete(`https://jsonplaceholder.typicode.com/users/${userid}`)
+          setAllData(allData.filter(user=>user.id!=userid))
+      } catch (error) {
+          console.log(error)
+      }
+  }
+  return (
+    <div>  <div className="row">
+    {allData.map((user) => (
+      <div key={user.id} className='card col-12 col-md-4 col-lg-2 m-4 p-4 bg-info'>
+          <div className='card-title'>Name: {user.name}</div>
+            <div className='card-title'>User Name: {user.username}</div>
+            <div className='card-title'>Email:{user.email}</div>
+            <div>Phone: {user.phone}</div>
+           <div> <button className='m-4 btn btn-primary' onClick={()=>handleDelete(user.id)}>Delete</button></div>
+        </div>
+    ))}      
+  </div></div>
+  )
+}
+
+export default DeleteUser
